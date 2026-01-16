1ee196b12bc521f8c7e404db4a67b315f2d84995
cazz <radicazz.dev@gmail.com>
2026-01-16 10:55:29 +0200

    refactor: adopt laya window wrapper

--- src/utils/window.cxx
+++ src/utils/window.cxx
@@ -2,102 +2,57 @@
 
 #include "../logger.hxx"
 
+#include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
-#include <SDL3_ttf/SDL_ttf.h>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
 namespace engine {
     game_window::game_window(std::string_view title, const glm::ivec2& size, game_window_type type)
-        : m_window(nullptr) {
-        SDL_WindowFlags window_flags;
-        switch (type) {
-            case game_window_type::resizable:
-                window_flags = SDL_WINDOW_RESIZABLE;
-                break;
-            case game_window_type::non_resizable:
-                window_flags = 0;
-                break;
-            case game_window_type::borderless:
-                window_flags = SDL_WINDOW_BORDERLESS;
-                break;
-            case game_window_type::fullscreen:
-                window_flags = SDL_WINDOW_FULLSCREEN;
-                break;
-            default:
-                window_flags = SDL_WINDOW_RESIZABLE;
-                break;
-        }
-
-        const std::string title_text{title};
-        if (m_window = SDL_CreateWindow(title_text.c_str(), size.x, size.y, window_flags);
-            m_window == nullptr) {
-            TTF_Quit();
-            SDL_Quit();
-            throw std::runtime_error("Failed to create window.");
-        }
-
+        : m_window(title,
+                   laya::dimensions{size.x, size.y},
+                   [&]() {
+                       switch (type) {
+                           case game_window_type::resizable:
+                               return laya::window_flags::resizable;
+                           case game_window_type::non_resizable:
+                               return laya::window_flags::none;
+                           case game_window_type::borderless:
+                               return laya::window_flags::borderless;
+                           case game_window_type::fullscreen:
+                               return laya::window_flags::fullscreen;
+                           default:
+                               return laya::window_flags::resizable;
+                       }
+                   }()),
+          m_title(title) {
         log_info("Window created: '{}' ({}x{})", title, size.x, size.y);
     }
 
-    game_window::~game_window() {
-        if (m_window != nullptr) {
-            SDL_DestroyWindow(m_window);
-            log_info("Window destroyed.");
-        }
-    }
-
-    game_window::game_window(game_window&& other) noexcept : m_window(other.m_window) {
-        other.m_window = nullptr;
-    }
-
-    game_window& game_window::operator=(game_window&& other) noexcept {
-        if (this != &other) {
-            if (m_window) {
-                SDL_DestroyWindow(m_window);
-            }
-            m_window = other.m_window;
-            other.m_window = nullptr;
-        }
-
-        return *this;
-    }
-
     std::string game_window::get_title() const {
-        return SDL_GetWindowTitle(m_window);
+        return m_title;
     }
 
     void game_window::set_title(std::string_view new_title) {
-        const std::string title_text{new_title};
-        if (SDL_SetWindowTitle(m_window, title_text.c_str()) == false) {
-            log_warning("Failed to set window title: {}", new_title);
-            return;
-        }
-
-        log_info("Window title set: {}", new_title);
+        m_title = std::string(new_title);
+        m_window.set_title(m_title);
+        log_info("Window title set: {}", m_title);
     }
 
     glm::ivec2 game_window::get_logical_size() const {
-        glm::ivec2 size = {0, 0};
-
-        if (SDL_GetWindowSize(m_window, &size.x, &size.y) == false) {
-            log_warning("Failed to get window size.");
-        }
-
-        return size;
+        const laya::dimensions size = m_window.get_size();
+        return {size.width, size.height};
     }
 
     void game_window::set_logical_size(const glm::ivec2& size) {
-        if (SDL_SetWindowSize(m_window, size.x, size.y) == false) {
-            log_warning("Failed to set window size: {}x{}", size.x, size.y);
-        }
+        m_window.set_size({size.x, size.y});
     }
 
     glm::ivec2 game_window::get_pixel_size() const {
         glm::ivec2 size = {0, 0};
 
-        if (SDL_GetWindowSizeInPixels(m_window, &size.x, &size.y) == false) {
+        if (SDL_GetWindowSizeInPixels(m_window.native_handle(), &size.x, &size.y) == false) {
             log_warning("Failed to get window pixel size.");
         }
 
@@ -119,7 +74,7 @@ namespace engine {
                 continue;
             }
 
-            if (SDL_SetWindowIcon(m_window, surface) == false) {
+            if (SDL_SetWindowIcon(m_window.native_handle(), surface) == false) {
                 SDL_DestroySurface(surface);
                 log_warning("Failed to set window icon from path: {}", path);
                 break;
--- src/utils/window.hxx
+++ src/utils/window.hxx
@@ -6,7 +6,7 @@
 #pragma once
 
 #include <string>
-#include <SDL3/SDL.h>
+#include <laya/windows/window.hpp>
 #include <glm/glm.hpp>
 
 namespace engine {
@@ -18,15 +18,17 @@ namespace engine {
     class game_window {
     public:
         explicit game_window(std::string_view title, const glm::ivec2& size, game_window_type type);
-        ~game_window();
+        ~game_window() = default;
 
         // Resource management - disable copy, enable move
         game_window(const game_window&) = delete;
         game_window& operator=(const game_window&) = delete;
-        game_window(game_window&& other) noexcept;
-        game_window& operator=(game_window&& other) noexcept;
+        game_window(game_window&& other) noexcept = default;
+        game_window& operator=(game_window&& other) noexcept = default;
 
         [[nodiscard]] SDL_Window* get_sdl_window() const;
+        [[nodiscard]] laya::window& get_laya_window() noexcept;
+        [[nodiscard]] const laya::window& get_laya_window() const noexcept;
 
         [[nodiscard]] std::string get_title() const;
         void set_title(std::string_view new_title);
@@ -43,10 +45,19 @@ namespace engine {
         void set_icon(std::string_view icon_path);
 
     private:
-        SDL_Window* m_window;
+        laya::window m_window;
+        std::string m_title;
     };
 
     inline SDL_Window* game_window::get_sdl_window() const {
+        return m_window.native_handle();
+    }
+
+    inline laya::window& game_window::get_laya_window() noexcept {
+        return m_window;
+    }
+
+    inline const laya::window& game_window::get_laya_window() const noexcept {
         return m_window;
     }
 }  // namespace engine
